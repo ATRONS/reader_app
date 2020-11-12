31cc14712111cc8d1bfcb4b246fe293a6815516c
mekedem <mekedemgetaneh@gmail.com>
2020-11-12 16:52:16 +0300

    changed default themeColor

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: Constants.appName,
-      theme: CustomTheme.darkTheme,
+      theme: CustomTheme.lightTheme,
       home: HomeScreen(),
     );
   }
