27890e52642b2c9e2e383cdec6aef81b59cfd5c0
mekedem <mekedemgetaneh@gmail.com>
2020-11-12 16:43:41 +0300

    initial commit

---
+++ lib/fragments/book_tab.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import './book_card.dart';
+import '../theme/app_theme.dart';
+
+class Booktab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _buildSectionTitle('Generes'),
+        SizedBox(height: 10.0),
+        _buildGenreSection(),
+        SizedBox(height: 20.0),
+        _buildNewSection(),
+        SizedBox(height: 10.0),
+      ],
+    );
+  }
+
+  _buildSectionTitle(String title) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            '$title',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildGenreSection() {
+    return Container(
+      height: 50.0,
+      child: Center(
+        child: ListView.builder(
+          primary: false,
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: 14,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            // Link link = homeProvider.top.feed.link[index];
+
+            // We don't need the tags from 0-9 because
+            // they are not categories
+            if (index < 10) {
+              return SizedBox();
+            }
+
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                ),
+                child: InkWell(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  onTap: () {
+                    // MyRouter.pushPage(
+                    //   context,
+                    //   Genre(
+                    //     title: '${link.title}',
+                    //     url: link.href,
+                    //   ),
+                    // );
+                  },
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'war and peace',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  _buildNewSection() {
+    return ListView.builder(
+      primary: false,
+      padding: EdgeInsets.symmetric(horizontal: 0.0),
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: 13,
+      itemBuilder: (BuildContext context, int index) {
+        if (index < 10) {
+          return SizedBox();
+        }
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          child: Column(
+            children: <Widget>[
+              _buildSectionHeader(),
+              SizedBox(height: 10.0),
+              _buildSectionBookList(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  _buildSectionHeader() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Flexible(
+            child: Text(
+              'Popular of Fiction',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              //   MyRouter.pushPage(
+              //     context,
+              //     Genre(
+              //       title: 'Crime and Punishment',
+              //       url: 'www.google.com',
+              //     ),
+              //   );
+            },
+            child: Text(
+              'See All',
+              style: TextStyle(
+                color: CustomTheme.lightAccent,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildSectionBookList() {
+    return Container(
+      height: 200.0,
+      child: Center(
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 5.0,
+                vertical: 10.0,
+              ),
+              child: BookCard(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
