7c41bb41c104053b87aad1e5452a92b9b974df53
mekedem <mekedemgetaneh@gmail.com>
2020-11-15 16:37:55 +0300

    list of the available catagories or genres are shown here

---
+++ lib/views/genres/listOfGenre.dart
@@ -0,0 +1,44 @@
+import 'package:atrons_mobile/models/genere.dart';
+import 'package:atrons_mobile/view_models/material_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ListOfGenre extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<MaterialProvider>(context, listen: false);
+    final generes = provider.generes;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Genres"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Divider(color: Colors.black38),
+            SizedBox(
+              height: 10,
+            ),
+            _buildGenreListSection(generes)
+          ],
+        ));
+  }
+
+  _buildGenreListSection(List<Genere> generes) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemCount: generes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              onTap: () {},
+              title: Text(generes[index].getGenereName(GenereLang.amharic)),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider();
+          },
+        ));
+  }
+}
