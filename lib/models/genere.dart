736b32af716b2507d833cea77b766a0261439a3f
mekedem <mekedemgetaneh@gmail.com>
2020-11-15 16:32:41 +0300

    added language filter method

--- lib/models/genere.dart
+++ lib/models/genere.dart
@@ -1,3 +1,5 @@
+enum GenereLang { english, amharic }
+
 class Genere {
   String id, name;
   List<dynamic> options;
@@ -6,4 +8,11 @@ class Genere {
       : id = json['_id'],
         name = json['name'],
         options = json['options'];
+
+  String getGenereName(GenereLang lang) {
+    final langStr = lang == GenereLang.english ? "english" : "amharic";
+    final chosen =
+        this.options.where((option) => option['lang'] == langStr).toList();
+    return chosen.length > 0 ? chosen[0]['value'] : "default";
+  }
 }
