3656305ff9bfdd6db1e23a3abc297ab97ffbedba
mekedem <mekedemgetaneh@gmail.com>
2020-11-15 12:15:02 +0300

    added login and signup pages ui

---
+++ lib/views/auth/login.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            _buildLogo(),
+            _buildAppName(),
+            SizedBox(
+              height: 20,
+            ),
+            _buildTextFields(),
+            SizedBox(
+              height: 50,
+            ),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: CircleAvatar(
+        backgroundColor: Colors.brown.shade800,
+        child: Text(''),
+        radius: 70,
+      ),
+    );
+  }
+
+  Widget _buildAppName() {
+    return Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(vertical: 5),
+        child: Text(
+          "ATRONS",
+          style: TextStyle(fontSize: 17),
+        ));
+  }
+
+  Widget _buildTextFields() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey[100]))),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Email",
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey[100]))),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Password",
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return Container(
+        height: 50,
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Theme.of(context).accentColor),
+        child: Center(
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white),
+          ),
+        ));
+  }
+}
