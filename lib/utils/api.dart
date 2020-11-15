905e301ef403941ee0de19e15e98f5327a467b9d
mekedem <mekedemgetaneh@gmail.com>
2020-11-15 16:31:10 +0300

    change in ip

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class Api {
   Dio dio = Dio();
-  static String baseUrl = 'http://10.6.152.134:5000/api/v1/';
+  static String baseUrl = 'http://10.6.210.6:5000/api/v1/';
   static String readerBaseUrl = baseUrl + '/reader';
   static String initialData = '/initialData';
 
