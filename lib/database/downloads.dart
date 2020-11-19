87bd82236cc2a3bd557a8608bfbc137a038be718
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-11-19 22:29:40 +0300

    initial commit

---
+++ lib/database/downloads.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:atrons_mobile/models/material.dart';
+import 'package:objectdb/objectdb.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DownloadsDB {
+  Future<String> getPath() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = documentDirectory.path + '/downloads.db';
+    return path;
+  }
+
+  void addMaterial(Map item) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    db.insert(item);
+    db.tidy();
+    await db.close();
+  }
+
+  Future<int> removeMaterial(String id) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    int val = await db.remove({'_id': id});
+    db.tidy();
+    await db.close();
+    return val;
+  }
+
+  Future<List<MiniMaterial>> getAllMaterials() async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    final val = List<dynamic>.from(await db.find({}))
+        .map((each) => MiniMaterial.fromJSON(each))
+        .toList();
+    db.tidy();
+    await db.close();
+    return val;
+  }
+
+  Future<List> findMaterials(Map query) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    List val = await db.find(query);
+    db.tidy();
+    await db.close();
+    return val;
+  }
+
+  void clear() async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    db.remove({});
+    db.close();
+  }
+}
