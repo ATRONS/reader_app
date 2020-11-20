daa0f2c4292e32228385af5d8b60391a5a3f0018
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-11-21 00:05:48 +0300

    initial commit

---
+++ lib/utils/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Style {
+  static final TextStyle whiteText =
+      TextStyle(color: Colors.white, fontSize: 12.0);
+
+  static final TextStyle blackTextH2 =
+      TextStyle(color: Colors.black, fontSize: 16.0);
+
+  static final TextStyle blackTextH1 =
+      TextStyle(color: Colors.black, fontSize: 18.0);
+}
