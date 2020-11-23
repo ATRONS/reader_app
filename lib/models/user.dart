289dcdffcc0c08bd67430d7cf62ff83c7235767d
mekedem <mekedemgetaneh@gmail.com>
2020-11-23 21:28:53 +0300

    created user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,13 @@
 class User {
   String key = 'abcdefghijklmnopqrstuvwxyzabcdef';
   String iv = 'abcdefghijklmnop';
-  String email = '';
   String phone = '';
+  String email, firstname, lastname, token, id;
+
+  User.fromJSON(Map<String, dynamic> json)
+      : id = json['_id'],
+        firstname = json['firstname'],
+        lastname = json['lastname'],
+        email = json['email'],
+        token = json['token'];
 }
