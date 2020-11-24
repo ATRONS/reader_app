66a5ad577f7729a8635a76b7dd23559f8d2f55f3
mekedem <mekedemgetaneh@gmail.com>
2020-11-24 10:21:12 +0300

    ipchange

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class Api {
   Dio dio = Dio();
-  static String baseUrl = 'http://192.168.43.240:5000/api/v1';
+  static String baseUrl = 'http://10.6.212.217:5000/api/v1';
   static String readerBaseUrl = baseUrl + '/reader';
   static String mediaBaseUrl = baseUrl + '/media';
 
