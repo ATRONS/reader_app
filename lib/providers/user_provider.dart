5f7364b29f5815ed43cf71e7a208ca7cdefd0169
mekedem <mekedemgetaneh@gmail.com>
2020-11-24 23:13:54 +0300

    added methods fetch user info and logout methods

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,3 +1,4 @@
+import 'package:atrons_mobile/database/readers.dart';
 import 'package:atrons_mobile/models/user.dart';
 import 'package:atrons_mobile/utils/api.dart';
 import 'package:dio/dio.dart';
@@ -8,13 +9,13 @@ class UserProvider extends ChangeNotifier {
   User _user;
   User get user => _user;
   final _api = Api();
+  final _readersDB = UsersDB();
   AuthenticationState signupStatus = AuthenticationState.unAuthenticated;
   AuthenticationState loginStatus = AuthenticationState.unAuthenticated;
 
-  Future fetchUserInfo() {
-    return Future.delayed(Duration(seconds: 1), () {
-      // _user = User();
-    });
+  Future fetchUserInfo() async {
+    final usrinfo = await _readersDB.getUser();
+    return usrinfo;
   }
 
   void signupUser(Map<String, String> credentials, dosignup) async {
@@ -32,6 +33,9 @@ class UserProvider extends ChangeNotifier {
       _user = User.fromJSON(body['data']['user_info']);
       _user.token = body['data']['token'];
       signupStatus = AuthenticationState.success;
+      final usr = _user.toUserJSON();
+
+      _readersDB.addUser(usr);
       dosignup();
       notifyListeners();
     }).catchError((err) {
@@ -58,6 +62,19 @@ class UserProvider extends ChangeNotifier {
       print(err);
       loginStatus = AuthenticationState.failed;
       notifyListeners();
+    });
+  }
+
+  void logoutUser(String token) async {
+    await _api.logoutReader(token).then((Response res) {
+      final Map<String, dynamic> body = res.data;
+      if (!body['success']) {
+        print('hard this shuttle');
+      }
+      print('loggedout successfully');
+      _readersDB.removeUser(token);
+    }).catchError((err) {
+      print(err);
     });
   }
 }
