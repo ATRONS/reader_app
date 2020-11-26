eb9d4b9f0c73de8cd2b6d36bfe281c884d46e7cf
mekedem <mekedemgetaneh@gmail.com>
2020-11-26 17:33:58 +0300

    first time commit on payment page - interface

---
+++ lib/views/payment/payment.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Choose Payment"),
+      ),
+      body: Center(
+        child: GridView.count(
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(4, (index) {
+            return Center(
+                child: InkWell(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Stack(
+                          overflow: Overflow.visible,
+                          children: <Widget>[
+                            Positioned(
+                              right: -40.0,
+                              top: -40.0,
+                              child: InkResponse(
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Container(),
+                              ),
+                            ),
+                            Form(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Enter Phone-Number",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: TextFormField(),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: RaisedButton(
+                                      child: Text("Submit"),
+                                      onPressed: () {},
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              },
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Image.network(
+                  'https://media.licdn.com/dms/image/C4D0BAQE_qTOpX0QhkA/company-logo_200_200/0?e=2159024400&v=beta&t=RbGbeQSvh9GAEINQKyWyvvFJv0khphM9LKefen3FRMc',
+                  fit: BoxFit.fill,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(
+                      color: index == 1 ? Colors.blue : Colors.transparent,
+                      width: 2,
+                    )),
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+              ),
+            ));
+          }),
+        ),
+      ),
+    );
+  }
+}
