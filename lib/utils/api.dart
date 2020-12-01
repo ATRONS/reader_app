0b9e288f07b963966d07e3799e16b77974b65fe7
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-01 16:08:43 +0300

    added token support for dio

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 
 class Api {
-  Dio dio = Dio();
-  static String baseUrl = 'http://10.6.212.217:5000/api/v1';
+  static final Dio _dio = Dio();
+  static String baseUrl = 'http://192.168.8.107:5000/api/v1';
   static String readerBaseUrl = baseUrl + '/reader';
   static String mediaBaseUrl = baseUrl + '/media';
 
@@ -14,31 +14,40 @@ class Api {
   static String loginUrl = readerBaseUrl + '/login';
   static String logoutUrl = readerBaseUrl + '/logout';
 
+  static setAuthToken(String token) {
+    _dio.options.headers['content-Type'] = 'application/json';
+    _dio.options.headers["Authorization"] = token;
+  }
+
   Future<Response> getInitialData() {
-    return dio.get(initialDataUrl);
+    print(_dio.options.headers);
+    return _dio.get(initialDataUrl);
   }
 
   Future<Response> getMaterials(String type) {
     final url = '$materialsUrl?type=$type';
-    return dio.get(url);
+    return _dio.get(url);
+  }
+
+  Future<Response> getProviders(String provides) {
+    final url = '$providersUrl?provides=$provides';
+    return _dio.get(url);
   }
 
   Future<Response> getMaterialDetail(String id) {
     final url = '$materialsUrl/$id';
-    return dio.get(url);
+    return _dio.get(url);
   }
 
   Future<Response> signupReader(Map<String, String> credentials) {
-    return dio.post(signupUrl, data: credentials);
+    return _dio.post(signupUrl, data: credentials);
   }
 
   Future<Response> loginReader(String email, String password) {
-    return dio.post('$loginUrl', data: {"email": email, "password": password});
+    return _dio.post('$loginUrl', data: {"email": email, "password": password});
   }
 
-  Future<Response> logoutReader(String token) {
-    dio.options.headers['content-Type'] = 'application/json';
-    dio.options.headers["authorization"] = token;
-    return dio.post('$logoutUrl', data: {});
+  Future<Response> logoutReader() {
+    return _dio.post('$logoutUrl');
   }
 }
