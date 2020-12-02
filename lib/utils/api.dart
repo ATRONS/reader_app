e4e8ca266603fa02e245ac67fb0ed53685fe6e64
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-02 11:39:40 +0300

    addef file download

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,8 +1,10 @@
+import 'package:atrons_mobile/models/material.dart';
+import 'package:atrons_mobile/utils/file_helper.dart';
 import 'package:dio/dio.dart';
 
 class Api {
   static final Dio _dio = Dio();
-  static String baseUrl = 'http://192.168.8.107:5000/api/v1';
+  static String baseUrl = 'http://192.168.43.113:5000/api/v1';
   static String readerBaseUrl = baseUrl + '/reader';
   static String mediaBaseUrl = baseUrl + '/media';
 
@@ -36,6 +38,7 @@ class Api {
 
   Future<Response> getMaterialDetail(String id) {
     final url = '$materialsUrl/$id';
+    print('get material detail called');
     return _dio.get(url);
   }
 
@@ -49,5 +52,25 @@ class Api {
 
   Future<Response> logoutReader() {
     return _dio.post('$logoutUrl');
+  }
+
+  void download(MaterialDetail material, Function onProgress, Function onFinish,
+      Function onError) async {
+    final fileurl = baseUrl + material.file['url'];
+    final imgUrl = material.coverImgUrl;
+    final downloadPath = await getEpubFilePath(material.id);
+    final imgDownloadPath = await getImgFilePath(material.id);
+
+    // download material epub file
+    await _dio
+        .download(fileurl, downloadPath,
+            deleteOnError: true, onReceiveProgress: onProgress)
+        .then((val) async {
+      // download cover_img
+      await _dio
+          .download(imgUrl, imgDownloadPath, deleteOnError: true)
+          .then((value) => onFinish())
+          .catchError(onError);
+    }).catchError(onError);
   }
 }
