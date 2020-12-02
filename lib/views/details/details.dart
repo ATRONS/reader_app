2d268a7b5ba79def69e30d2027ef15f3690a588c
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-02 22:44:18 +0300

    redirecting to purchase page on buy/rent click [instead of downloading]

--- lib/views/details/details.dart
+++ lib/views/details/details.dart
@@ -1,10 +1,12 @@
 import 'package:atrons_mobile/models/material.dart';
 import 'package:atrons_mobile/utils/constants.dart';
 import 'package:atrons_mobile/utils/file_helper.dart';
+import 'package:atrons_mobile/utils/router.dart';
 import 'package:atrons_mobile/utils/styles.dart';
 import 'package:atrons_mobile/providers/detail_provider.dart';
 import 'package:atrons_mobile/providers/loading_state.dart';
 import 'package:atrons_mobile/providers/material_provider.dart';
+import 'package:atrons_mobile/views/purchase/purchase.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../fragments/book_list_item.dart';
@@ -244,7 +246,9 @@ class _DetailsState extends State<Details> {
                         );
 
                         final downloadBtn = FlatButton(
-                          onPressed: () => detailProvider.downloadFile(context),
+                          onPressed: () {
+                            MyRouter.pushPage(context, Purchase());
+                          },
                           child: Padding(
                             padding: const EdgeInsets.symmetric(vertical: 4.0),
                             child: Text(
