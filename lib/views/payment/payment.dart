1ab96d953d5c401733153db1d100473570489dad
hiskelkelemework <hiskelkelemework6@gmail.com>
2020-12-03 09:22:29 +0300

    converted payment page to statful widget

--- lib/views/payment/payment.dart
+++ lib/views/payment/payment.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  int _selectedPaymentIndex;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,79 +17,91 @@ class PaymentPage extends StatelessWidget {
       body: Center(
         child: GridView.count(
           crossAxisCount: 2,
-          // Generate 100 widgets that display their index in the List.
-          children: List.generate(4, (index) {
-            return Center(
+          children: List.generate(
+            3,
+            (index) {
+              return Center(
                 child: InkWell(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        content: Stack(
-                          overflow: Overflow.visible,
-                          children: <Widget>[
-                            Positioned(
-                              right: -40.0,
-                              top: -40.0,
-                              child: InkResponse(
-                                onTap: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: Container(),
-                              ),
-                            ),
-                            Form(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      "Enter Phone-Number",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: TextFormField(),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: RaisedButton(
-                                      child: Text("Submit"),
-                                      onPressed: () {},
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
+                  onTap: () {
+                    setState(() {
+                      _selectedPaymentIndex = index;
                     });
-              },
-              child: Card(
-                semanticContainer: true,
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: Image.asset(
-                  'assets/images/warandpeace.jpg',
-                  fit: BoxFit.fill,
+                    _showPhoneDialog(context);
+                  },
+                  child: Card(
+                    semanticContainer: true,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Image.asset(
+                      'assets/images/hellocash.jpg',
+                      fit: BoxFit.fill,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: BorderSide(
+                          color: index == _selectedPaymentIndex
+                              ? Colors.blue
+                              : Colors.transparent,
+                          width: 2,
+                        )),
+                    elevation: 5,
+                    margin: EdgeInsets.all(10),
+                  ),
                 ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    side: BorderSide(
-                      color: index == 1 ? Colors.blue : Colors.transparent,
-                      width: 2,
-                    )),
-                elevation: 5,
-                margin: EdgeInsets.all(10),
-              ),
-            ));
-          }),
+              );
+            },
+          ),
         ),
       ),
+    );
+  }
+
+  _showPhoneDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              Positioned(
+                right: -40.0,
+                top: -40.0,
+                child: InkResponse(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(),
+                ),
+              ),
+              Form(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Enter Phone-Number",
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextFormField(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RaisedButton(
+                        child: Text("Submit"),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
