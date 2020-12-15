40ecf5fdf97a82f6431180bf46ac61075e39c794
mekedem <mekedemgetaneh@gmail.com>
2020-12-16 00:03:22 +0300

    fixed rating update on detail page

--- lib/fragments/rate_material.dart
+++ lib/fragments/rate_material.dart
@@ -26,99 +26,106 @@ class RatePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final detailProvider = Provider.of<DetailProvider>(context, listen: false);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Rating"),
-        actions: [
-          Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: InkWell(
-                onTap: () {
-                  detailProvider.setMaterialRating(
-                      myratingvalue, mycomment, materialId);
+    return WillPopScope(
+      onWillPop: () async {
+        detailProvider.resetStateAndReloadDetail();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Rating"),
+          actions: [
+            Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: InkWell(
+                  onTap: () {
+                    detailProvider.setMaterialRating(
+                        myratingvalue, mycomment, materialId);
+                  },
+                  child: Selector<DetailProvider, LoadingState>(
+                      selector: (context, model) => model.setRatingState,
+                      builder: (context, state, child) {
+                        return state == LoadingState.uninitialized
+                            ? Text(
+                                "POST",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            : state == LoadingState.success
+                                ? Container(
+                                    child: Icon(
+                                      Icons.check,
+                                      color: Colors.green,
+                                    ),
+                                  )
+                                : Container(
+                                    width: 18,
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2.0,
+                                      valueColor:
+                                          AlwaysStoppedAnimation(Colors.blue),
+                                    ),
+                                  );
+                      }),
+                ))
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: SmoothStarRating(
+                  allowHalfRating: false,
+                  onRated: (v) {
+                    myratingvalue = v.toInt();
+                  },
+                  starCount: 5,
+                  rating: lastrate == null ? 0 : lastrate['value'].toDouble(),
+                  size: 50.0,
+                  isReadOnly: false,
+                  filledIconData: Icons.star,
+                  halfFilledIconData: Icons.star_half,
+                  color: Colors.green,
+                  borderColor: Colors.green,
+                  spacing: 0.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: TextField(
+                maxLength: 200,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0),
+                    ),
+                    hintText: 'Describe your experience (optional)'),
+                onChanged: (value) {
+                  mycomment = value;
                 },
-                child: Selector<DetailProvider, LoadingState>(
-                    selector: (context, model) => model.loadingState,
-                    builder: (context, state, child) {
-                      return state == LoadingState.uninitialized
-                          ? Text(
-                              "POST",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )
-                          : state == LoadingState.success
-                              ? Container(
-                                  child: Icon(
-                                    Icons.check,
-                                    color: Colors.green,
-                                  ),
-                                )
-                              : Container(
-                                  width: 18,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2.0,
-                                    valueColor:
-                                        AlwaysStoppedAnimation(Colors.blue),
-                                  ),
-                                );
-                    }),
-              ))
-        ],
-      ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-            child: SmoothStarRating(
-                allowHalfRating: false,
-                onRated: (v) {
-                  myratingvalue = v.toInt();
-                },
-                starCount: 5,
-                rating: lastrate == null ? 0 : lastrate['value'].toDouble(),
-                size: 50.0,
-                isReadOnly: false,
-                filledIconData: Icons.star,
-                halfFilledIconData: Icons.star_half,
-                color: Colors.green,
-                borderColor: Colors.green,
-                spacing: 0.0),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: TextField(
-              maxLength: 200,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 3.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 3.0),
-                  ),
-                  hintText: 'Describe your experience (optional)'),
-              onChanged: (value) {
-                mycomment = value;
+              ),
+            ),
+            addVerticalSpace(10),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: comments.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 12.0),
+                  child: MaterialReview(
+                      firstname: comments[index].username['firstname'],
+                      lastname: comments[index].username['lastname'],
+                      comment: comments[index].comment,
+                      stars: comments[index].starvalue),
+                );
               },
             ),
-          ),
-          addVerticalSpace(10),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: comments.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 12.0),
-                child: MaterialReview(
-                    firstname: comments[index].username['firstname'],
-                    lastname: comments[index].username['lastname'],
-                    comment: comments[index].comment,
-                    stars: comments[index].starvalue),
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
