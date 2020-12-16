cd4b197f2eb791aec2cf7466b17612a8974bf1fd
mekedem <mekedemgetaneh@gmail.com>
2020-12-16 09:34:07 +0300

    left global state

--- lib/views/companyMaterials/company_material_view.dart
+++ lib/views/companyMaterials/company_material_view.dart
@@ -1,11 +1,12 @@
 import 'package:atrons_mobile/fragments/company_material_card.dart';
+import 'package:atrons_mobile/models/material.dart';
 import 'package:atrons_mobile/providers/loading_state.dart';
 import 'package:atrons_mobile/providers/material_provider.dart';
-import 'package:atrons_mobile/utils/constants.dart';
+import 'package:atrons_mobile/utils/helper_funcs.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CompanyMaterialView extends StatelessWidget {
+class CompanyMaterialView extends StatefulWidget {
   // loadListOfCompanyMaterials
   final String materialId;
   final String title;
@@ -13,56 +14,98 @@ class CompanyMaterialView extends StatelessWidget {
   CompanyMaterialView(this.materialId, this.title);
 
   @override
-  Widget build(BuildContext context) {
-    final provider = Provider.of<MaterialProvider>(context, listen: false);
-    provider.loadListOfCompanyMaterials(materialId);
+  _CompanyMaterialViewState createState() => _CompanyMaterialViewState();
+}
 
+class _CompanyMaterialViewState extends State<CompanyMaterialView> {
+  List<MiniMaterial> _materials;
+
+  LoadingState loadingState = LoadingState.failed;
+
+  @override
+  void initState() {
+    loadCompanyMaterials(context);
+    super.initState();
+  }
+
+  void loadCompanyMaterials(BuildContext context) async {
+    final provider = Provider.of<MaterialProvider>(context, listen: false);
+    setState(() {
+      loadingState = LoadingState.loading;
+    });
+    await provider
+        .loadListOfCompanyMaterials(widget.materialId)
+        .then((materials) {
+      _materials = materials;
+      setState(() {
+        loadingState = LoadingState.success;
+      });
+    }).catchError((err) {
+      setState(() {
+        loadingState = LoadingState.failed;
+      });
+    });
+  }
+
+  Widget _buildReloadButton() {
+    return Center(
+      child: Column(
+        children: [
+          Text('Loading failed'),
+          addVerticalSpace(10),
+          FlatButton(
+            onPressed: () {
+              loadCompanyMaterials(context);
+            },
+            child: Text('Retry'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildEmptyMessage() {
+    return Center(child: Text('No materials found'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Selector<MaterialProvider, LoadingState>(
-          selector: (context, model) => model.materialListLoadingState,
-          builder: (context, state, child) {
-            if (state == LoadingState.loading) {
-              return Center(child: CircularProgressIndicator());
-            }
-
-            if (state == LoadingState.failed) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(Constants.loadingFailed),
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text(Constants.retry),
-                    ),
-                  ],
-                ),
-              );
-            }
-            return GridView.builder(
-              // physics: new NeverScrollableScrollPhysics(),
-              padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
-              shrinkWrap: true,
-              itemCount: provider.listOfCompanyMaterials.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 200 / 340,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 5.0),
-                  child: CompanyMaterialCard(
-                      minimaterial: provider.listOfCompanyMaterials[index]),
-                );
-              },
-            );
-          },
-        ),
-      ),
+          child: loadingState == LoadingState.failed
+              ? _buildReloadButton()
+              : loadingState == LoadingState.loading
+                  ? _buildLoading()
+                  : _materials.length == 0
+                      ? _buildEmptyMessage()
+                      : GridView.builder(
+                          // physics: new NeverScrollableScrollPhysics(),
+                          padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+                          shrinkWrap: true,
+                          itemCount: _materials.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: 200 / 340,
+                          ),
+                          itemBuilder: (BuildContext context, int index) {
+                            final material = _materials[index];
+                            return Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5.0),
+                              child:
+                                  CompanyMaterialCard(minimaterial: material),
+                            );
+                          },
+                        )),
     );
   }
 }
